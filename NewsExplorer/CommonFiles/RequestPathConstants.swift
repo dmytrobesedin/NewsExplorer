f633f2c07f562f98780b0a03fe2e189f18aa23d1
Dmytro <dmytro.besedin@gmail.com>
2023-08-10 12:14:48 +0300

    change apiKey

--- NewsExplorer/CommonFiles/RequestPathConstants.swift
+++ NewsExplorer/CommonFiles/RequestPathConstants.swift
@@ -10,7 +10,7 @@ import Foundation
 class RequestPathConstants {
     static let baseURL = "https://newsapi.org/v2/everything?"
     static let techCrunchDomain = "domains=techcrunch.com"
-    static let apiKey = "&apiKey=58c544cc327c415b88d8cb4fda7749a2"
+    static let apiKey = "&apiKey=b028e072018d448c856442309d8c9cf2"
     static func getStringURL(from: String, to: String) -> String {
         return baseURL + RequestPathConstants.techCrunchDomain + "&from=\(from)&to=\(to)" + apiKey
     }
